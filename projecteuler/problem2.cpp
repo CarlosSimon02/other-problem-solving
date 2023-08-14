6d031ead477f17a508b94b253152df18983b988b
CarlosSimon02 <caloycamacho02@gmail.com>
2023-08-14 20:16:05 +0800

    1 Commit 2023 Aug 8

--- projecteuler/problem2.cpp
+++ projecteuler/problem2.cpp
@@ -2,5 +2,22 @@
 
 int main() 
 {
-    std::cout << "moderfucker" << std::endl;    
+    int prev1Term = 0;
+    int prev2Term = 1;
+    int currentTerm = prev1Term + prev2Term;
+    int sum = 0;
+
+    while(currentTerm <= 4'000'000)
+    {
+        if(currentTerm % 2 == 0)
+        {
+            sum += currentTerm;
+        }
+
+        prev1Term = prev2Term;
+        prev2Term = currentTerm;
+        currentTerm = prev1Term + prev2Term;
+    }
+
+    std::cout << sum << std::endl;
 }
