5d70b0783954fa1ceba92436d948173185cbfed5
CarlosSimon02 <caloycamacho02@gmail.com>
2023-08-15 07:59:19 +0800

    2 commit 2023 aug 15

--- projecteuler/problem3.cpp
+++ projecteuler/problem3.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-// #include <cmath>
+#include <cmath>
 
 bool isPrime(int num)
 {
     if(num <= 1) return false;
 
-    for(int i = 2; i < num; i++)
+    for(int i = 2; i <= round(sqrt(num)); i++)
         if(num % i == 0) return false;
 
     return true;
@@ -13,5 +13,15 @@ bool isPrime(int num)
 
 int main() 
 {
-    std::cout << isPrime(36) ;
+    int largestPrime = 0;
+    for(int i = round(sqrt(600851475143)); i > 2; i--)
+    {
+        if(600851475143 % i == 0 && isPrime(i))
+        {
+            largestPrime = i;
+            break;
+        }
+    }
+
+    std::cout << largestPrime << std::endl;
 }
